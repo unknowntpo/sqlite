66b2ad3d7a90025c1ace5f3d5fca4cff8b6d1a2d
張辰謙 <unknowntpo@unknowntpo-MacBook-Pro.local>
2022-08-06 19:50:11 +0800

    why serialize_row does not work ?
    Value are all zero.

--- src/db.c
+++ src/db.c
@@ -87,6 +87,8 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
 ExecuteResult execute_select(Statement *statement, Table *table)
 {
     Row row;
+    printf("row->id: [%d]\trow->username: [%s]\trow->email: [%s]\n", row.id,
+           row.username, row.email);
     for (uint32_t i = 0; i < table->num_rows; i++) {
         deserialize_row(row_slot(table, i), &row);
         print_row(&row);
--- test/db_test.cpp
+++ test/db_test.cpp
@@ -37,3 +37,33 @@ TEST(testInputBuffer, read_input)
     EXPECT_EQ(b->input_length, strlen(want));
     EXPECT_STREQ(b->buffer, want);
 }
+
+TEST(testSerializeData, serialize)
+{
+    // FILE *f = fopen("./testdata/input_buffer.txt", "r");
+    // InputBuffer *b = new_input_buffer();
+    // read_input(b, f);
+    // char *want = "insert 1 user1 person1@example.com";
+    Row row = {.id = 1, .username = "Alice", .email = "alice@example.com"};
+    char *dest = (char *) malloc(sizeof(Row) + 1);
+    // set terminator
+    memset(&dest[sizeof(Row)], '\0', 1);
+    EXPECT_TRUE(dest);
+    print_row(&row);
+    serialize_row(&row, dest);
+
+    dest[0] = 'h';
+    dest[1] = 'e';
+
+    printf("dest: %s\n", dest);
+
+    // char *ptr = (char *) dest;
+    // int i = 0;
+    // while (ptr) {
+    //     printf("dest[%d]: %c\n", i++, (*ptr));
+    //     ptr++;
+    // }
+
+    // char *want = "lala";
+    // EXPECT_STREQ(dest, strlen(want));
+}
